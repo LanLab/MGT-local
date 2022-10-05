c27ab9a6d8e9512553d70a786dbf22a3bc83215e
mjohnpayne <mjohnpayne@gmail.com>
2022-10-05 14:15:47 +1100

    fix settings_template.py

--- Mgt/Mgt/Mgt/settings_template.py
+++ Mgt/Mgt/Mgt/settings_template.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '' #CHANGE add random string
 
