1c5076f771d8394001a6bfaedfedcb0e8c15fc73
mjohnpayne <mjohnpayne@gmail.com>
2022-10-05 14:01:50 +1100

    fix settings_template.py

--- Mgt/Mgt/Mgt/settings_template.py
+++ Mgt/Mgt/Mgt/settings_template.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '' #CHANGE add random string
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Mgt.urls.urls_template'
+ROOT_URLCONF = 'Mgt.urls_template' # CHANGE to new urls filename
 
 TEMPLATES = [
     {
